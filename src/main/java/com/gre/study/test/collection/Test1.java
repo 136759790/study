39cab280ea26893fa5c7c072957483b1ce9d1edb
rp1 <136759790@qq.com>
2018-09-26 17:34:04 +0800

    init

---
+++ src/main/java/com/gre/study/test/collection/Test1.java
@@ -0,0 +1,38 @@
+package com.gre.study.test.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.junit.Test;
+
+/**
+ * 集合框架
+ * @author zxt
+ * 
+ */
+public class Test1 {
+	
+	
+	
+	/**
+	 * 集合框架集成关系
+	 * 								Collection
+	 * 			List					SET					Queue
+	 * 	ArrayList	LinkedList		  HashSet				
+	 */
+	@Test
+	public void test0(){
+		Collection c1 = new ArrayList();
+		Collection c2 = new LinkedList();
+		Collection c3 = new HashSet();
+	}
+	
+	
+	
+	
+	
+	
+}
